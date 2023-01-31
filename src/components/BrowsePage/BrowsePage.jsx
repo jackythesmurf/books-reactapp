2f0896c479f6b7025423e21748cd1ea1123c4cbf
jackythesmurf <Jackythesmurf@gmail.com>
2023-01-31 11:53:34 +1100

    browse books design

--- src/components/BrowsePage/BrowsePage.jsx
+++ src/components/BrowsePage/BrowsePage.jsx
@@ -1,8 +1,32 @@
-import styles from "./BrowsePage.module.scss"
-const BrowsePage = ({ }) => {
-    return (
-        <>
-        </>
-    )
-}
-export default BrowsePage
+import styles from "./BrowsePage.module.scss";
+const BrowsePage = ({}) => {
+
+    const listOfBookURL = ["https://cdn2.penguin.com.au/covers/original/9781857150704.jpg",
+                        "https://cdn2.penguin.com.au/covers/original/9781857150353.jpg",
+                    "https://cdn2.penguin.com.au/covers/original/9781857150629.jpg",
+                    "https://cdn2.penguin.com.au/covers/original/9780141192093.jpg"]
+	return (
+		<div>
+			<h3>My Recommendations</h3>
+			<div className={styles.articles}>
+				<img 
+					src="https://cdn2.penguin.com.au/covers/original/9781857150704.jpg"
+					alt=""
+					width="300"
+				/>
+				<img
+					width="300"
+					src="https://cdn2.penguin.com.au/covers/original/9781857150353.jpg"
+					alt=""
+				/>
+				<img
+					width="300"
+					src="https://cdn2.penguin.com.au/covers/original/9781857150629.jpg"
+					alt=""
+				/>
+                <img width="315" src="https://cdn2.penguin.com.au/covers/original/9780141192093.jpg" alt="" />
+			</div>
+		</div>
+	);
+};
+export default BrowsePage;
